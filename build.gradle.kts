62ffc46a5bb3be3e753d2be3fc22ac3a860bc21d
necasper <roman4uk.nick@yandex.ru>
2026-05-31 00:23:39 +0300

    step 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
 	java
+	checkstyle
 	id("org.springframework.boot") version "4.0.6"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "7.3.0.8198"
 }
 
 group = "hexlet.code"
@@ -23,6 +25,27 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+dependencyLocking {
+	lockAllConfigurations()
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+sonar {
+	properties {
+		val projectKey = System.getenv("SONAR_PROJECT_KEY")?.takeIf { it.isNotBlank() }
+			?: "necasper_java-project-99"
+		val organization = System.getenv("SONAR_ORGANIZATION")?.takeIf { it.isNotBlank() }
+			?: "necasper"
+		property("sonar.projectKey", projectKey)
+		property("sonar.organization", organization)
+		property("sonar.host.url", "https://sonarcloud.io")
+		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+		System.getenv("SONAR_TOKEN")?.takeIf { it.isNotBlank() }?.let { token ->
+			property("sonar.token", token)
+			property("sonar.login", token)
+		}
+	}
 }
